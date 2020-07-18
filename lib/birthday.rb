d756c51f169d5b2899cfd8cea837247169603ed5
john beltrez <beltrezjohn@gmail.com>
2020-07-18 13:19:03 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,4 +15,6 @@ def age_appropriate_birthday(birthday_kids)
   if kids_name > 12 
     puts "You are too old for this."
   else 
-    puts "Happy Birthday "
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end 
