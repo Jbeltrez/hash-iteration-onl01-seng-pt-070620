3daf80462a9289190fc78c834488dbad26bc4e09
john beltrez <beltrezjohn@gmail.com>
2020-07-18 13:16:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    end # only prints
+    end # only prints out happy birthday with age if kid is 12 or younger
 end
 
 def age_appropriate_birthday(birthday_kids)
